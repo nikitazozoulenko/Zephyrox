755f383141318adbe61336eeda44086a0a4c235c
Nikita Zozoulenko <nikita.zozoulenko@gmail.com>
2024-05-06 19:00:29 +0100

    ran some prelim TSC, now some prelim TSER

--- run_tser.py
+++ run_tser.py
@@ -17,19 +17,20 @@ from utils import print_name, print_shape
 
 from preprocessing.timeseries_augmentation import normalize_mean_std_traindata, normalize_streams, augment_time, add_basepoint_zero
 from aeon.classification.sklearn import RotationForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import RidgeClassifierCV
+from sklearn.metrics import root_mean_squared_error
 
-jax.config.update('jax_platform_name', 'gpu') # Used to set the platform (cpu, gpu, etc.)
+jax.config.update('jax_platform_name', 'cpu') # Used to set the platform (cpu, gpu, etc.)
 np.set_printoptions(precision=3, threshold=5) # Print options
 
-from aeon.datasets.tsc_datasets import multivariate, univariate, univariate_equal_length
-from aeon.datasets import load_classification
+from aeon.datasets.tser_datasets import tser_soton
+from aeon.datasets import load_regression
 
+from sklearn.linear_model import RidgeCV
+from aeon.transformations.collection.convolution_based import Rocket, MultiRocketMultivariate, MiniRocketMultivariate
 
 def get_aeon_dataset(
         dataset_name:str, 
-        extract_path = "/home/nikita/hdd/Data/TSC/"
+        extract_path = "/home/nikita/hdd/Data/TSER/"
         ):
     """Loads a dataset from the UCR/UEA archive using 
     the aeon library.
@@ -40,8 +41,8 @@ def get_aeon_dataset(
     Returns:
         Tuple: 4-tuple of the form (X_train, y_train, X_test, y_test)
     """
-    X_train, y_train = load_classification(dataset_name, split="train", extract_path=extract_path)
-    X_test, y_test = load_classification(dataset_name, split="test", extract_path=extract_path)
+    X_train, y_train = load_regression(dataset_name, split="train", extract_path=extract_path)
+    X_test, y_test = load_regression(dataset_name, split="test", extract_path=extract_path)
 
     return X_train.transpose(0,2,1), y_train, X_test.transpose(0,2,1), y_test
 
@@ -71,19 +72,18 @@ def train_and_test_sigbased(
     t1 = time.time()
 
     # train classifier      
-    clf = RidgeClassifierCV(alphas=np.logspace(-6, -1, 20))
+    clf = RidgeCV(alphas=np.logspace(-6, -1, 20))
     clf.fit(feat_train_X, train_y)
     t2 = time.time()
 
     # predict
     pred = clf.predict(feat_test_X)
-    acc = accuracy_score(test_y, pred)
+    rmse = root_mean_squared_error(test_y, pred)
     t3 = time.time()
-    return acc, t1-t0, t2-t1, t3-t2, clf.alpha_
+    return rmse, t1-t0, t2-t1, t3-t2, clf.alpha_
 
 
-from sklearn.linear_model import RidgeClassifierCV
-from aeon.transformations.collection.convolution_based import Rocket, MultiRocket, MiniRocket
+
 
 def train_and_test_ROCKETS(
         train_X, train_y, test_X, test_y,
@@ -102,21 +102,21 @@ def train_and_test_ROCKETS(
     t1 = time.time()
 
     # train classifier      
-    clf = RidgeClassifierCV(alphas=np.logspace(-6, -1, 20))
+    clf = RidgeCV(alphas=np.logspace(-6, -1, 20))
     clf.fit(feat_train_X, train_y)
     t2 = time.time()
 
     # predict
     pred = clf.predict(feat_test_X)
-    acc = accuracy_score(test_y, pred)
+    rmse = root_mean_squared_error(test_y, pred)
     t3 = time.time()
-    return acc, t1-t0, t2-t1, t3-t2, clf.alpha_
+    return rmse, t1-t0, t2-t1, t3-t2, clf.alpha_
 
 
 def run_all_experiments(X_train, y_train, X_test, y_test):
     prng_key = jax.random.PRNGKey(999)
     max_batch = 32
-    trunc_level = 5
+    trunc_level = 4
     n_features = 1000
 
     jax_models = [
@@ -147,32 +147,32 @@ def run_all_experiments(X_train, y_train, X_test, y_test):
     numpy_seed = 99
     rocket_models = [
         ["Rocket", Rocket(n_features//2, random_state=numpy_seed)],
-        ["MiniRocket", MiniRocket(n_features, random_state=numpy_seed)],
-        ["MultiRocket", MultiRocket(n_features//4, random_state=numpy_seed)],
+        ["MiniRocket", MiniRocketMultivariate(n_features, random_state=numpy_seed)],
+        ["MultiRocket", MultiRocketMultivariate(n_features//4, random_state=numpy_seed)],
         ]
     
     # Run experiments
-    accs = []
+    RMSEs = []
     alphas = []
     model_names = []
     #jax
     for name, model in jax_models:
         model_names.append(name)
-        acc, t_trans, t_fit, t_pred, alpha = train_and_test_sigbased(
+        rmse, t_trans, t_fit, t_pred, alpha = train_and_test_sigbased(
             X_train, y_train, X_test, y_test, model
             )
         alphas.append(alpha)
-        accs.append(acc)
+        RMSEs.append(rmse)
     #numpy
     for name, model in rocket_models:
         model_names.append(name)
-        acc, t_trans, t_fit, t_pred, alpha = train_and_test_ROCKETS(
+        rmse, t_trans, t_fit, t_pred, alpha = train_and_test_ROCKETS(
             X_train, y_train, X_test, y_test, model
             )
         alphas.append(alpha)
-        accs.append(acc)
+        RMSEs.append(rmse)
     
-    return model_names, accs, alphas
+    return model_names, RMSEs, alphas
 
 
 def do_experiments(datasets: List[str]):
@@ -184,6 +184,7 @@ def do_experiments(datasets: List[str]):
             print(dataset_name)
             X_train, y_train, X_test, y_test = get_aeon_dataset(dataset_name)
             X_train, X_test = normalize_streams(X_train, X_test, max_T=1000)
+            y_train, y_test = normalize_mean_std_traindata(y_train, y_test)
             N_train = X_train.shape[0]
             N_test = X_test.shape[0]
             T = X_train.shape[1]
@@ -198,33 +199,28 @@ def do_experiments(datasets: List[str]):
                 "D": D,
             }
             experiments[dataset_name] = results, 
-            print(dataset_name)
-            print(results)
-            print("\n")
         except Exception as e:
             print(f"Error: {e}")
             failed[dataset_name] = e
     return experiments, experiments_metadata, failed
 
-
-
 if __name__ == "__main__":
     #run experiments
-    d_res, d_meta, d_failed = do_experiments(list(univariate_equal_length))
+    d_res, d_meta, d_failed = do_experiments(list(tser_soton))
 
     # make dict of results
     model_names = d_res[list(d_res.keys())[0]][0][0]
     alpha_names = ["alpha_" + model_name for model_name in model_names]
-    df_accs = pd.DataFrame({dataset : accs for dataset, ((model_names, accs, alphas),) in d_res.items()}).transpose()
-    df_accs.columns = model_names
-    df_alphas = pd.DataFrame({dataset : alphas for dataset, ((model_names, accs, alphas),) in d_res.items()}).transpose()
+    df_RMSEs = pd.DataFrame({dataset : RMSEs for dataset, ((model_names, RMSEs, alphas),) in d_res.items()}).transpose()
+    df_RMSEs.columns = model_names
+    df_alphas = pd.DataFrame({dataset : alphas for dataset, ((model_names, RMSEs, alphas),) in d_res.items()}).transpose()
     df_alphas.columns = alpha_names
     meta = pd.DataFrame(d_meta).transpose()
 
-    df_accs = pd.concat([meta, df_accs], axis=1)
+    df_RMSEs = pd.concat([meta, df_RMSEs], axis=1)
     df_alphas = pd.concat([meta, df_alphas], axis=1)
 
     # save
-    df_accs.to_pickle("df_accs_univariateTSC.pkl")
-    df_accs.to_pickle("df_alphas_univariateTSC.pkl")
+    df_RMSEs.to_pickle("df_RMSEs_TSER.pkl")
+    df_alphas.to_pickle("df_alphas_TSER.pkl")
     print(d_failed)
